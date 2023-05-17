dbbe59127d93482373389691f08a8ae4bb59d437
Stepan-a11y <kostko52@gmail.com>
2023-05-17 18:03:10 +0300

    delyty

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const connectDB = require('./connectDB');
+const Guests = require('./schemas/guestsSchema');
 
 
 const serv = express();
+
+const router = express.Router();
+
 const port = process.env.PORT || 3001;
+
+
+router.get('/', (req, res) => {
+    res.json({
+        "hello": "hi!"
+    })
+})
+
+
+router.post( '/newguest', (req, res) => {
+        let newGuest = new Guests({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            status: req.body.status,
+        });
+        
+        try {
+            newGuest.save();
+            res.json(newGuest);    
+        } catch (err) {
+            res.json({success: false, msg: "not add"});
+        }
+});
 
 serv.use(cors());
 serv.use(bodyParser.json());
 
-const newGuest = require('./routes/newGuest')
-
-serv.use('/api', newGuest) 
+serv.use('/api', router) 
 
 serv.listen(port, ()=>{
     console.log("connected on port" + port);
